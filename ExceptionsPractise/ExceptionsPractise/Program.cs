b85dd6f8e76e815c17fd63a45b1b93784a887aef
fidanmardanli <fidannm@code.edu.az>
2022-07-18 00:53:27 +0400

    Tasks about Exceptions

---
+++ ExceptionsPractise/ExceptionsPractise/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace ExceptionsPractise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //try
+            //{
+            //	//int a = 5;
+            //	//int b = 0;
+            //	//var result = a / b;
+
+            //	int[] arr = new int[3];
+            //	arr[7] = 8;
+
+
+
+            //}
+            //catch (Exception ex)
+            //{
+
+            //	Console.WriteLine(ex.Message);
+            //}
+            //finally
+            //         {
+            //             Console.WriteLine("salamos");
+            //         }
+
+
+
+            //Divide(4, 2);
+
+
+            //List<string> list = new List<string>() { "orxan", "fidash", "mahmud", "lalesh" };
+            //string searchText = "fidash";
+            //FindName(list, searchText); 
+
+            
+
+        }
+
+        //public static void Divide(int a, int b)
+        //{
+        //    try
+        //    {
+        //        if ( b==2 )
+        //        {
+        //            throw new DivideExceptions("sehvdi zaa");
+        //        }
+        //        Console.WriteLine(a/b);
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //}
+
+        //public static void FindName(List<string> names, string name)
+        //{
+        //    try
+        //    {
+        //        //if (!names.Contains(name))
+        //        //{
+        //        //    throw new FindingName("Yoxdu");
+        //        //}
+        //        //Console.WriteLine("var");
+
+        //        var data = names.Find(m => m == name);
+        //        if (data is null) throw new FindingName("yoxdu zzaa");
+        //        Console.WriteLine("var zuuz");
+        //    }
+            
+        //    catch (Exception ex)
+        //    {
+
+        //        Console.WriteLine(ex.Message);
+        //    }
+        //}
+
+       
+
+
+    }
+
+    class DivideExceptions : Exception
+    {
+        public DivideExceptions(string message) :base(message)
+        {
+
+        }
+
+    }
+
+    class FindingName : Exception
+    {
+        public FindingName(string message) : base(message)
+        {
+
+        }
+
+    }
+
+    
+
+
+}
---
+++ LoginExceptionTask/LoginExceptionTask/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LoginExceptionTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Login("fidash.13", 12345);
+        }
+
+        public static void Login(string name, int pascode)
+        {
+            try
+            {
+                string n = "fidash.13";
+                int m = 12345;
+
+                if (pascode == m && (name == n))
+                {
+                    throw new Account("Girish olundu");
+                }
+                Console.WriteLine("Girish olunmadi");
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
